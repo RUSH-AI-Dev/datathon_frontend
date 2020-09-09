952fec0663d76ac17b4065c7d2d564d57f40f142
RUSH-AI-Dev <rush.ai.dev@gmail.com>
2020-09-09 21:56:23 +0700

    so good.

--- src/services/constants.js
+++ src/services/constants.js
@@ -4,11 +4,11 @@ export const NETWORK_TIMEOUT_MESSAGE = "A network timeout has occurred, Please t
 export const UPLOAD_PHOTO_FAIL_MESSAGE = "An error has occurred. The photo was unable to upload.";
 export const NOT_CONNECT_NETWORK = "NOT_CONNECT_NETWORK";
 
-export const apiUrl = "http://localhost:3000/api/v2";
-export const imageUrl = "http://localhost:3000/images";
+// export const apiUrl = "http://localhost:3000/api/v2";
+// export const imageUrl = "http://localhost:3000/images";
 
-// export const apiUrl = "https://a6972f3dc68b.ngrok.io/api/v2";
-// export const imageUrl = "https://a6972f3dc68b.ngrok.io/images";
+export const apiUrl = "https://55d04e85b869.ngrok.io/api/v2";
+export const imageUrl = "https://55d04e85b869.ngrok.io/images";
 
 export const server = {
   LOGIN_URL: `login`,
